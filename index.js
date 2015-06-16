04edfccc41e3f886e5e025d0fc148c1e88e1f1ec
Kristian Trenskow <trenskow@me.com>
2015-06-16 23:05:36 +0200

    Moved exposed route to index.js.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-exports = module.exports = require('./lib/smart-static');
+var SmartStatic = require('./lib/smart-static');
+
+// We expose a route instead of the actual class.
+exports = module.exports = function(root, opt) {
+  var smartStatic = new SmartStatic(root, opt);
+  return smartStatic.route.bind(smartStatic);
+};
+
+// Expose class as well
+exports.SmartStatic = SmartStatic;
