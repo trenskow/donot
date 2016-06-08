9f9cf7d3724cfcc659f3365c1aa799d91c6ea88a
Kristian Trenskow <trenskow@me.com>
2016-06-08 02:50:04 +0200

    Support for source maps.

--- test/test.js
+++ test/test.js
@@ -36,66 +36,66 @@ function createServer(transform, opt) {
 
 }
 
-describe('Donot', function() {
+describe('Donot', () => {
 
-	describe('constructing', function() {
+	describe('constructing', () => {
 
-		it ('should throw TypeError if root is not supplied', function() {
-			expect(function() {
+		it ('should throw TypeError if root is not supplied', () => {
+			expect(() => {
 				new Donot();
 			}).to.throw(TypeError);
 		});
 
-		it ('should throw TypeError if root is not a string', function() {
-			expect(function() {
+		it ('should throw TypeError if root is not a string', () => {
+			expect(() => {
 				new Donot(1);
 			}).to.throw(TypeError);
 		});
 
-		it ('should throw TypeError if opt is not an object', function() {
-			expect(function() {
+		it ('should throw TypeError if opt is not an object', () => {
+			expect(() => {
 				new Donot("", 1);
 			}).to.throw(TypeError);
 		});
 
-		it ('should throw Error if root does not exist', function() {
-			expect(function() {
+		it ('should throw Error if root does not exist', () => {
+			expect(() => {
 				new Donot(__dirname + '/not-exist');
 			}).to.throw(Error);
 		});
 
-		it ('should throw Error if root is not a directory', function() {
-			expect(function() {
+		it ('should throw Error if root is not a directory', () => {
+			expect(() => {
 				new Donot(__dirname + '/data/static.txt');
 			}).to.throw(Error);
 		});
 
-		it ('should throw TypeError if cache does not inherit from Cache', function() {
-			expect(function() {
+		it ('should throw TypeError if cache does not inherit from Cache', () => {
+			expect(() => {
 				new Donot(__dirname + '/data/static.txt', {
 					cache: {}
 				});
 			}).to.throw(TypeError);
 		});
 
-		it ('should throw a TypeError if serveDir is not a string', function() {
-			expect(function() {
+		it ('should throw a TypeError if serveDir is not a string', () => {
+			expect(() => {
 				new Donot('', {
 					serveDir: true
 				});
 			}).to.throw(TypeError);
 		});
 
-		it('should throw a TypeError if accessControl is not an object', function() {
-			expect(function() {
+		it('should throw a TypeError if accessControl is not an object', () => {
+			expect(() => {
 				new Donot('', {
 					accessControl: true
 				});
 			}).to.throw(TypeError);
 		});
 
-		it ('should throw a TypeError if both allow and deny is set on accessControl', function() {
-			expect(function() {
+		it ('should throw a TypeError if both allow and deny is set on accessControl', () => {
+			expect(() => {
 				new Donot('', {
 					accessControl: {
 						allow: [],
@@ -105,8 +105,8 @@ describe('Donot', function() {
 			}).to.throw(TypeError);
 		});
 
-		it ('should throw a TypeError if accessControl.allow isn\'t an array', function() {
-			expect(function() {
+		it ('should throw a TypeError if accessControl.allow isn\'t an array', () => {
+			expect(() => {
 				new Donot('', {
 					accessControl: {
 						allow: true
@@ -115,8 +115,8 @@ describe('Donot', function() {
 			}).to.throw(TypeError);
 		});
 
-		it ('should throw a TypeError if accessControl.deny isn\'t an array', function() {
-			expect(function() {
+		it ('should throw a TypeError if accessControl.deny isn\'t an array', () => {
+			expect(() => {
 				new Donot('', {
 					accessControl: {
 						deny: true
@@ -125,8 +125,8 @@ describe('Donot', function() {
 			}).to.throw(TypeError);
 		});
 
-		it ('should throw a TypeError if accessControl.deny contains non-string or non-regexp', function() {
-			expect(function() {
+		it ('should throw a TypeError if accessControl.deny contains non-string or non-regexp', () => {
+			expect(() => {
 				new Donot('', {
 					accessControl: {
 						deny: [true]
@@ -135,8 +135,8 @@ describe('Donot', function() {
 			}).to.throw(TypeError);
 		});
 
-		it ('should throw a TypeError if accessControl.allow contains non-string or non-regexp', function() {
-			expect(function() {
+		it ('should throw a TypeError if accessControl.allow contains non-string or non-regexp', () => {
+			expect(() => {
 				new Donot('', {
 					accessControl: {
 						allow: [true]
@@ -145,139 +145,165 @@ describe('Donot', function() {
 			}).to.throw(TypeError);
 		});
 
-		it ('should return an instance', function() {
+		it ('should return an instance', () => {
 			expect(new Donot(__dirname + '/data')).to.be.instanceof(Donot);
 		});
 
 	});
 
-	describe('adding transforms', function() {
+	describe('adding transforms', () => {
 
 		var ss;
-		before(function() {
+		before(() => {
 			ss = new Donot(__dirname + '/data');
 		});
 
-		it ('should throw TypeError on missing transform', function() {
-			expect(function() {
+		it ('should throw TypeError on missing transform', () => {
+			expect(() => {
 				ss.transform();
 			}).to.throw(TypeError);
 		});
 
-		it ('should throw TypeError if transform is not an instance of Transform', function() {
-			expect(function() {
+		it ('should throw TypeError if transform is not an instance of Transform', () => {
+			expect(() => {
 				ss.transform({});
 			}).to.throw(TypeError);
 		});
 
 	});
 
-	describe('renderer', function() {
+	describe('renderer', () => {
 
 		var ss;
 
-		before(function() {
+		before(() => {
 			ss = new Donot(__dirname + '/data', {
 				transforms: [new TestTransform()]
 			});
 		});
 
-		it ('should throw TypeError if file name is missing', function() {
+		it ('should throw TypeError if file name is missing', () => {
 			return ss.render().should.eventually.be.rejected;
 		});
 
-		it ('should throw TypeError if file name is not a string', function() {
+		it ('should throw TypeError if file name is not a string', () => {
 			return ss.render(1).should.eventually.be.rejected;
 		});
 
 	});
 
-	describe('route', function() {
+	describe('route', () => {
 
 		var testTransform = new TestTransform();
 		var reverseTransform = new ReverseTransform();
 
-		describe('common', function() {
+		describe('common', () => {
 
 			var server;
-			before(function() {
-				server = createServer([testTransform, reverseTransform]);
+			before(() => {
+				server = createServer([reverseTransform, testTransform]);
 			});
 
-			it ('should return 404 on not found', function(done) {
+			it ('should return 404 on not found', (done) => {
 				request(server)
 				.get('/not-found.txt')
 				.expect(404, 'not found', done);
 			});
 
-			it ('should return index on directory', function(done) {
+			it ('should return index on directory', (done) => {
 				request(server)
 				.get('/')
 				.expect(200, 'index test\n', done);
 			});
 
-			it ('should return 404 on non-existing files without extension', function(done) {
+			it ('should return 404 on non-existing files without extension', (done) => {
 				request(server)
 				.get('/static')
 				.expect(404, 'not found', done);
 			});
 
-			it ('should return 404 on non-GET requests', function(done) {
+			it ('should return 404 on non-GET requests', (done) => {
 				request(server)
 				.post('/static.txt')
 				.expect(404, 'not found', done);
 			});
 
-			it ('should return 200 on HEAD requests', function(done) {
+			it ('should return 200 on HEAD requests', (done) => {
 				request(server)
 				.head('/static.txt')
 				.expect(200, done);
 			});
 
-			it ('should send static files', function(done) {
+			it ('should send static files', (done) => {
 				request(server)
 				.get('/static.txt')
 				.expect('Content-Type', 'text/plain; charset=UTF-8')
 				.expect(200, 'This is a static file.\n', done);
 			});
 
-			it ('should send rendered template file', function(done) {
+			it ('should send rendered template file', (done) => {
 				request(server)
 				.get('/template.txt')
 				.expect('Content-Type', 'text/plain; charset=UTF-8')
 				.expect(200, 'this is a template\ntest', done);
 			});
 
-			it ('should send chained rendered template file', function(done) {
+			it ('should send chained rendered template file', (done) => {
 				request(server)
 				.get('/template.reversed.txt')
 				.expect('Content-Type', 'text/plain; charset=UTF-8')
 				.expect(200, 'tset\netalpmet a si siht', done);
 			});
 
-			it ('should send rendered index template file', function(done) {
+			it ('should send rendered index template file', (done) => {
 				request(server)
 				.get('/sub')
 				.expect('Content-Type', 'text/plain; charset=UTF-8')
 				.expect(200, 'index\ntest', done);
 			});
 
-			describe('serveDir', function() {
+			describe('source maps', () => {
 
 				var server;
-				before(function() {
+				before(() => {
+					server = createServer([
+						new (require('@donotjs/donot-transform-minify'))(),
+						new (require('@donotjs/donot-transform-es5'))()
+					]);
+				});
+
+				it ('should come back with es5\'ed minified data', (done) => {
+					request(server)
+					.get('/test.es5.min.js')
+					.expect('Content-Type', 'application/javascript; charset=UTF-8')
+					.expect(200, '"use strict";!function(){console.log("test")}();\n//# sourceMappingURL=test.es5.js', done);
+				});
+
+				it ('should come back with a source map', (done) => {
+					request(server)
+					.get('/test.es5.min.js.map')
+					.expect('Content-Type', 'application/octet-stream; charset=UTF-8')
+					.expect(200, done);
+				});
+
+			});
+
+			describe('serveDir', () => {
+
+				var server;
+				before(() => {
 					server = createServer(testTransform, {
 						serveDir: '/test'
 					});
 				});
 
-				it ('should come back with 404 if not in path', function(done) {
+				it ('should come back with 404 if not in path', (done) => {
 					request(server)
 					.get('/template.txt')
 					.expect(404, done);
 				});
 
-				it ('should come back with 200 if in path', function(done) {
+				it ('should come back with 200 if in path', (done) => {
 					request(server)
 					.get('/test/template.txt')
 					.expect(200, 'this is a template\ntest', done);
@@ -285,10 +311,10 @@ describe('Donot', function() {
 
 			});
 
-			describe('cache control', function() {
+			describe('cache control', () => {
 
 				var headers;
-				before(function(done) {
+				before((done) => {
 					(new Donot(__dirname + '/data', {
 						transforms: [testTransform]
 					})).render('/template.txt').then((result) => {
@@ -300,7 +326,7 @@ describe('Donot', function() {
 					});
 				});
 
-				it ('should have correct cache headers set', function(done) {
+				it ('should have correct cache headers set', (done) => {
 					request(server)
 					.get('/template.txt')
 					.expect('Last-Modified', headers.lastModified.toUTCString())
@@ -308,14 +334,14 @@ describe('Donot', function() {
 					.expect(200, done);
 				});
 
-				it ('should return 304 on If-None-Match', function(done) {
+				it ('should return 304 on If-None-Match', (done) => {
 					request(server)
 					.get('/template.txt')
 					.set('If-None-Match', headers.etag)
 					.expect(304, done);
 				});
 
-				it ('should return 200 on non-matching If-None-Match', function(done) {
+				it ('should return 200 on non-matching If-None-Match', (done) => {
 					request(server)
 					.get('/template.txt')
 					.set('If-None-Match', 'non-matching-tag')
@@ -326,42 +352,42 @@ describe('Donot', function() {
 
 		});
 
-		describe('access', function() {
+		describe('access', () => {
 
-			describe('hidden files', function() {
+			describe('hidden files', () => {
 
-				describe('restrictive', function() {
+				describe('restrictive', () => {
 
 					var server;
-					before(function() {
+					before(() => {
 						server = createServer(testTransform, {
 							etag: false,
 							lastModified: false
 						});
 					});
 
-					it ('should return 404 on hidden files', function(done) {
+					it ('should return 404 on hidden files', (done) => {
 						request(server)
 						.get('/.hidden.txt')
 						.expect(404, 'not found', done);
 					});
 
-					it ('should return 404 on hidden directories', function(done) {
+					it ('should return 404 on hidden directories', (done) => {
 						request(server)
 						.get('/.hidden/test.txt')
 						.expect(404, 'not found', done);
 					});
 
-					it ('should return 404 on template files', function(done) {
+					it ('should return 404 on template files', (done) => {
 						request(server)
 						.get('/template.test')
 						.expect(404, 'not found', done);
 					});
 
-					it ('should not cache headers', function(done) {
+					it ('should not cache headers', (done) => {
 						request(server)
 						.get('/template.txt')
-						.expect(function(res) {
+						.expect((res) => {
 							if (res.header['last-modified'] !== undefined) {
 								return "Last-Modified should not be set";
 							}
@@ -374,29 +400,29 @@ describe('Donot', function() {
 
 				});
 
-				describe('permissive', function() {
+				describe('permissive', () => {
 
 					var server;
-					before(function() {
+					before(() => {
 						server = createServer(testTransform, {
 							dotFiles: true,
 							templates: true
 						});
 					});
 
-					it ('should send hidden files', function(done) {
+					it ('should send hidden files', (done) => {
 						request(server)
 						.get('/.hidden.txt')
 						.expect(200, 'hidden\n', done);
 					});
 
-					it ('should send file in hidden directories', function(done) {
+					it ('should send file in hidden directories', (done) => {
 						request(server)
 						.get('/.hidden/test.txt')
 						.expect(200, 'hidden directory\n', done);
 					});
 
-					it ('should send template files', function(done) {
+					it ('should send template files', (done) => {
 						request(server)
 						.get('/template.test')
 						.expect(200, 'test this is a template\n', done);
@@ -406,12 +432,12 @@ describe('Donot', function() {
 
 			});
 
-			describe('access control', function() {
+			describe('access control', () => {
 
-				describe('deny (String - file ext)', function() {
+				describe('deny (String - file ext)', () => {
 
 					var server;
-					before(function() {
+					before(() => {
 						server = createServer(testTransform, {
 							accessControl: {
 								deny: ['.txt']
@@ -419,19 +445,19 @@ describe('Donot', function() {
 						});
 					});
 
-					it ('should come back with 404', function(done) {
+					it ('should come back with 404', (done) => {
 						request(server)
 						.get('/index.txt')
 						.expect(404, done);
 					});
 
-					it ('should come back with 404', function(done) {
+					it ('should come back with 404', (done) => {
 						request(server)
 						.get('/template.txt')
 						.expect(404, done);
 					});
 
-					it ('should come back with 200', function(done) {
+					it ('should come back with 200', (done) => {
 						request(server)
 						.get('/test.allow')
 						.expect(200, 'allow this\n', done);
@@ -439,10 +465,10 @@ describe('Donot', function() {
 
 				});
 
-				describe('allow (RegExp)', function() {
+				describe('allow (RegExp)', () => {
 
 					var server;
-					before(function() {
+					before(() => {
 						server = createServer(testTransform, {
 							accessControl: {
 								allow: [/^.*?\.allow$/]
@@ -450,19 +476,19 @@ describe('Donot', function() {
 						});
 					});
 
-					it ('should come back with 404', function(done) {
+					it ('should come back with 404', (done) => {
 						request(server)
 						.get('/index.txt')
 						.expect(404, done);
 					});
 
-					it ('should come back with 404', function(done) {
+					it ('should come back with 404', (done) => {
 						request(server)
 						.get('/template.txt')
 						.expect(404, done);
 					});
 
-					it ('should come back with 200', function(done) {
+					it ('should come back with 200', (done) => {
 						request(server)
 						.get('/test.allow')
 						.expect(200, 'allow this\n', done);
@@ -476,18 +502,18 @@ describe('Donot', function() {
 
 	});
 
-	describe('cache', function() {
+	describe('cache', () => {
 
 		var ss;
 		var cache = new MemoryCache();
-		before(function() {
+		before(() => {
 			ss = new Donot(__dirname + '/data', {
 				transforms: [new TestTransform()],
 				cache: cache
 			});
 		});
 
-		it ('should compile and render template', function() {
+		it ('should compile and render template', () => {
 			return ss.render('/template.txt').then((result) => {
 				expect(result).to.be.an('object');
 				expect(result.data.toString()).to.equal('this is a template\ntest');
@@ -495,7 +521,7 @@ describe('Donot', function() {
 			}).should.eventually.be.fulfilled;
 		});
 
-		it ('should render template from cache', function() {
+		it ('should render template from cache', () => {
 			return ss.render('/template.txt').then((result) => {
 				expect(result).to.be.an('object');
 				expect(result.data.toString()).to.equal('this is a template\ntest');
@@ -503,9 +529,9 @@ describe('Donot', function() {
 			}).should.eventually.be.fulfilled;
 		});
 
-		describe('after cache invalidated', function() {
+		describe('after cache invalidated', () => {
 
-			before(function(done) {
+			before((done) => {
 				// Get the modified date of template and set back cache one year
 				fs.stat(path.normalize(__dirname + '/data/template.test'), function(err, stats) {
 					expect(err).to.be.null;
@@ -516,7 +542,7 @@ describe('Donot', function() {
 				});
 			});
 
-			it ('should recompile and render template', function() {
+			it ('should recompile and render template', () => {
 				return ss.render('/template.txt').then((result) => {
 					expect(result).to.be.an('object');
 					expect(result.data.toString()).to.equal('this is a template\ntest');
